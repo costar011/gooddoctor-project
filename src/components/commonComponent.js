d7d61872b82e6497afb94aa53d06dd822dc0d651
YERIM <dnaer456@gmail.com>
2021-02-02 21:47:04 +0900

    footer

---
+++ src/components/commonComponent.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: ${(props) => props.width || `100%`};
+  min-width: ${(props) => props.minWidth};
+  height: ${(props) => props.height};
+  min-height: ${(props) => props.minHeight};
+  display: ${(props) => props.display || `flex`};
+  flex-direction: ${(props) => props.dr || `column`};
+  align-items: ${(props) => props.al || `center`};
+  justify-content: ${(props) => props.ju || `center`};
+  flex-wrap: ${(props) => props.wrap || `wrap`};
+  background: ${(props) => props.bgColor};
+  color: ${(props) => props.color};
+  position: ${(props) => (props.isAbsolute ? `absolute` : ``)};
+  position: ${(props) => (props.isRelative ? `relative` : ``)};
+  top: ${(props) => props.top};
+  left: ${(props) => props.left};
+  bottom: ${(props) => props.bottom};
+  right: ${(props) => props.right};
+  z-index: ${(props) => props.zIndex};
+  border: ${(props) => props.border};
+  border-bottom: ${(props) => props.borderBottom};
+  border-top: ${(props) => props.borderTop};
+  border-right: ${(props) => props.borderRight};
+  border-left: ${(props) => props.borderLeft};
+  border-radius: ${(props) => props.radius};
+  box-shadow: ${(props) => props.shadow};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  overflow: ${(props) => (props.isOverflow ? `auto` : props.overflow || ``)};
+  background-image: ${(props) => props.bgImg};
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: ${(props) => props.attachment || `fixed`};
+  background-position: center;
+  transition: 0.5s;
+  cursor: ${(props) => props.cursor};
+  line-height: ${(props) => props.lineHeight};
+  text-align: ${(props) => props.textAlign};
+  & pre {
+    white-space: pre-wrap;
+    line-height: 1.4;
+    color: #6f6f6f;
+    font-size: 14px;
+  }
+  @media (max-width: 700px) {
+    font-size: 14px;
+    & svg {
+      font-size: 20px;
+    }
+  }
+`;
--- src/routers/layouts/Footer.jsx
+++ src/routers/layouts/Footer.jsx
@@ -1,8 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "../../img/hoscoc.png";
+import { Wrapper } from "../../components/commonComponent";
+
+const FooterWrapper = styled.div`
+  height: 35vh;
+  margin-top: 40px;
+
+  background-color: ${(props) => props.theme.mainThemeColor};
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const FooterTxt = styled.p`
+  font-size: 15px;
+  color: #ececec;
+  margin: 10px 0px 10px 100px;
+`;
 
 const Footer = () => {
-  return `Footer`;
+  return (
+    <FooterWrapper>
+      <Wrapper height={`100%`} width={`200px`}>
+        <LogoBox>
+          <img src={logo} width="300px" height="230px" />
+        </LogoBox>
+      </Wrapper>
+
+      <Wrapper height={`100%`} width={`1000px`} al={`center`} ju={`center`}>
+        <FooterTxt>(주) 4leaf / 충남 공주시 우금티로 530</FooterTxt>
+      </Wrapper>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
