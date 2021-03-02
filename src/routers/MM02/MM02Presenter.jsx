d6c9793137d068bf87c779cb9037a127e2f01bcd
rlarudals <gim181004@gmail.com>
2021-03-02 21:39:16 +0900

    intro create

--- src/routers/MM02/MM02Presenter.jsx
+++ src/routers/MM02/MM02Presenter.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
+import content1 from "../../img/content1.png";
+import content2 from "../../img/content2.png";
+import content3 from "../../img/content3.png";
+import content4 from "../../img/content4.png";
+
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
@@ -17,12 +22,25 @@ const TopWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-top: 130px;
 `;
 
 const BottomWrapper = styled.div`
   width: 100%;
   height: 80vh;
   display: flex;
+  flex-direction: row;
+`;
+
+const ImgWrapper = styled.div`
+  width: 100%;
+  height: 20vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 60px;
+  margin-top: 150px;
 `;
 
 const ContentWrapper = styled.div`
@@ -35,41 +53,74 @@ const ContentWrapper = styled.div`
 `;
 
 const ContentText = styled.span`
-  font-size: 20px;
+  font-size: 17px;
   font-weight: 800;
   color: #546574;
   padding: 5px;
-  margin: 10px;
+  margin-top: 5px;
 `;
 
 const MainText = styled.span`
-  font-size: 30px;
+  font-size: 35px;
   font-style: oblique;
   font-weight: 800;
   color: #8393a0;
-  padding: 5px;
+  padding: 20px;
   margin: 10px;
+`;
+
+const TopText = styled.span`
+  font-size: 25px;
+  font-weight: 800;
+  color: #546574;
+  padding: 5px;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  height: 190px;
+  opacity: 0.7;
 `;
 
 const MM02Presenter = () => {
   return (
     <Wrapper>
       <TopWrapper>
-        <ContentText>Hospital appointment System</ContentText>
+        <TopText>Hospital appointment System</TopText>
         <MainText>Hoscoc</MainText>
         <ContentWrapper>
           <ContentText>
-            안녕하세요 저는 존예존귀 김경민 입니다. 오늘은 효진군과 예림군이
-            바보인 이유를 설명해 보겠습니다.
+            인터넷을 통한 진료예약으로 대기 시간을 절약해 보세요!
           </ContentText>
           <ContentText>
-            일단 효진군은 걍 어떻게 하면 얼타기 때문이고, 예림군은 하는짓이
-            (푸흡) 초딩 같기 때문입니다.
+            방문 전에 진료시간, 병원소재, 의료원, 진료내용 등 병원에 대한 정보를
+            미리 확인할 수 있습니다.
           </ContentText>
-          <ContentText>이상 존예존귀 김경민 이었습니다.</ContentText>
         </ContentWrapper>
       </TopWrapper>
-      <BottomWrapper></BottomWrapper>
+
+      <BottomWrapper>
+        <ImgWrapper>
+          <Image src={content1} />
+          <TopText>ijewfjwef</TopText>
+          <ContentText>diwiwfeffeffnfuew</ContentText>
+        </ImgWrapper>
+        <ImgWrapper>
+          <Image src={content2} />
+          <TopText>efwefef</TopText>
+          <ContentText>diwiwfefeefneflewnfewf</ContentText>
+        </ImgWrapper>
+        <ImgWrapper>
+          <Image src={content3} />
+          <TopText>fwefwfwef</TopText>
+          <ContentText>diwiwfeffefjfoiwefe</ContentText>
+        </ImgWrapper>
+        <ImgWrapper>
+          <Image src={content4} />
+          <TopText>gdsgg</TopText>
+          <ContentText>diwiwfeffuwfklfeflqkne</ContentText>
+        </ImgWrapper>
+      </BottomWrapper>
     </Wrapper>
   );
 };
--- src/routers/MM03/MM03Presenter.jsx
+++ src/routers/MM03/MM03Presenter.jsx
@@ -1,7 +1,119 @@
 import React from "react";
+import styled from "styled-components";
+import Fade from "react-reveal/Fade";
+import introImg from "../../img/introImg.png";
+
+const MainWrapper = styled.div`
+  width: ${(props) => props.width || `100%`};
+  height: ${(props) => props.height || `100%`};
+
+  padding: ${(props) => props.padding || ``};
+
+  display: flex;
+  flex-direction: column;
+
+  justify-content: flex-start;
+`;
+
+const ColWrapper = styled.div`
+  width: ${(props) => props.width || `100%`};
+  height: ${(props) => props.height || `100%`};
+  margin: 0px 0px 50px 80px;
+  padding: ${(props) => props.padding || ``};
+
+  display: flex;
+  flex-direction: row;
+`;
+
+const SpanWrapper = styled.div`
+  width: ${(props) => props.width || `48%`};
+  height: ${(props) => props.height || `100%`};
+  margin: 40px 40px 40px 40px;
+  padding: 0px 0px 30px 0px;
+
+  display: flex;
+  flex-direction: row;
+
+  border-bottom: 7px #83adbd solid;
+`;
+
+const TextWrapper = styled.div`
+  width: ${(props) => props.width || `100%`};
+  height: ${(props) => props.height || `100%`};
+  margin: 60px 0px 20px 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const EmjText = styled.span`
+  font-size: 30px;
+  font-weight: bold;
+  margin: 0px 0px 0px 30px;
+`;
+
+const SpanTextMain = styled.span`
+  font-size: 30px;
+  font-weight: bold;
+  color: white;
+  text-shadow: 2px 8px 4px #60a3bc, 0px 3px 10px #0a3d62;
+  margin: 0px 0px 0px 10px;
+`;
+
+const SpanText = styled.span`
+  font-size: 20px;
+  font-weight: bold;
+  color: #808080;
+`;
+
+const SpanTextL = styled.span`
+  font-size: 25px;
+  font-weight: bold;
+  color: white;
+  text-shadow: 2px 8px 4px #60a3bc, 0px 3px 10px #0a3d62;
+  margin-bottom: 20px;
+`;
 
 const MM03Presenter = () => {
-  return `MM03`;
+  return (
+    <MainWrapper>
+      <SpanWrapper>
+        <Fade>
+          <EmjText>🏥</EmjText>
+          <SpanTextMain>Hoscoc과 건강을 예약하세요!</SpanTextMain>
+        </Fade>
+      </SpanWrapper>
+      <ColWrapper>
+        <Fade left>
+          <img src={introImg} width="700px" height="700px" />
+        </Fade>
+
+        <TextWrapper>
+          <Fade right>
+            <TextWrapper>
+              <SpanTextL>병원 예약 플랫폼 HOSCOC!</SpanTextL>
+              <SpanText>
+                전국의 병원 진료를 HOSCOC을 이용해 예약하세요!
+              </SpanText>
+            </TextWrapper>
+            <TextWrapper>
+              <SpanTextL>간편한 진료예약,</SpanTextL>
+              <SpanText>언제 어디서나 쉽고 빠르게 예약하세요!</SpanText>
+            </TextWrapper>
+            <TextWrapper>
+              <SpanTextL>요즘 같은 언택트 시대!</SpanTextL>
+              <SpanText>함께 건강을 예약해봐요!</SpanText>
+            </TextWrapper>
+            <TextWrapper>
+              <SpanTextL>건강한 생활을 예약하는 HOSCOC!</SpanTextL>
+              <SpanText>병원 예약 웹 제작! 바로 만나보세요</SpanText>
+            </TextWrapper>
+          </Fade>
+        </TextWrapper>
+      </ColWrapper>
+    </MainWrapper>
+  );
 };
 
 export default MM03Presenter;
